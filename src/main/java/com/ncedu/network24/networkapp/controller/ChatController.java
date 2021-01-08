3a28ed8a0529cb20305e7622151e9dbd2044ae85
n0tre <mwxj@mail.ru>
2021-01-08 22:37:19 +0400

    + Chat + Background & Style

---
+++ src/main/java/com/ncedu/network24/networkapp/controller/ChatController.java
@@ -0,0 +1,33 @@
+package com.ncedu.network24.networkapp.controller;
+
+import com.ncedu.network24.networkapp.model.ChatMessage;
+import com.ncedu.network24.networkapp.repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @Autowired
+    private UserRepo userRepo;
+
+   @MessageMapping("/chat.register")
+   @SendTo("/topic/public")
+    public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.send")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+       return chatMessage;
+    }
+
+
+
+}
