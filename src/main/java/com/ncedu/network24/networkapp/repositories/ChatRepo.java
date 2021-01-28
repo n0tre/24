b821994de3da40535012bb540717daf26b6db6b8
n0tre <mwxj@mail.ru>
2021-01-28 11:57:21 +0400

    + change query

--- src/main/java/com/ncedu/network24/networkapp/repositories/ChatRepo.java
+++ src/main/java/com/ncedu/network24/networkapp/repositories/ChatRepo.java
@@ -2,7 +2,6 @@ package com.ncedu.network24.networkapp.repositories;
 
 import com.ncedu.network24.networkapp.domain.Chat;
 import com.ncedu.network24.networkapp.domain.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -11,25 +10,16 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Set;
 
-
+@Repository
 public interface ChatRepo extends CrudRepository<Chat, Long> {
-
 
 
     Chat findChatByFirstUserIdAndSecondUserId(Long firstUserId, Long secondUserId);
 
 
-    Set<Chat> findChatByFirstUserId(Long userId);
-
-    Set<Chat> findChatBySecondUserId(Long userId);
-
-    List<Chat> findAllByFirstUserId(Long userId);
-
-    List<Chat> findAllBySecondUserId(Long userId);
-
-    @Query("SELECT DISTINCT u FROM Chat as c INNER JOIN User as u ON c.firstUserId = u.id OR c.secondUserId = u.id")
+    @Query("SELECT DISTINCT u FROM Chat as c INNER JOIN fetch User as u ON c.firstUserId = u.id OR c.secondUserId = u.id")
     @Modifying
-    public List<User> listOfChats(Long firstUserId, Long secondUserId);
+    List<User> listOfChats(Long firstUserId, Long secondUserId);
 
 
 
